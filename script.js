34679bd7580fa2170b58f43b3bd2f54998cc6e75
“Hemant <harshselkar789@gmail.com>
2025-09-21 10:27:50 +0530

    changes js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const navbar = document.getElementById("navbar");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
+  if (window.scrollY > 30) {
     navbar.classList.add("scrolled");
   } else {
     navbar.classList.remove("scrolled");
